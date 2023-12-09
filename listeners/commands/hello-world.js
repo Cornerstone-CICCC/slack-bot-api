aac1af94daf9f745f0c138508f6fd3188388d8ec
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-12-08 21:56:14 -0800

    fix error hello world action button

--- listeners/commands/hello-world.js
+++ listeners/commands/hello-world.js
@@ -21,7 +21,7 @@ const helloWorldCallback = async ({ ack, payload, context, respond }) => {
               type: "plain_text",
               text: "Click me!",
             },
-            action_id: "sample_action_id",
+            action_id: "button_abc",
           },
         },
       ],
