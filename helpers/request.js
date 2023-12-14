221d52912e708bb69c2e4dbc50d7b2aa7adb8aef
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-12-13 18:38:21 -0800

    split post and get request

--- helpers/request.js
+++ helpers/request.js
@@ -1,4 +1,22 @@
-const req = async (endpoint, method, body = {}) => {
+const getReq = async (endpoint) => {
+  const url = `${process.env.API_URL}/${endpoint}`;
+  const headers = new Headers();
+  headers.set(
+    "Authorization",
+    "Basic " +
+      Buffer.from(
+        process.env.API_USERNAME + ":" + process.env.API_PASSWORD
+      ).toString("base64")
+  );
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers,
+  });
+  return response.json();
+};
+
+const postReq = async (endpoint, body) => {
   const url = `${process.env.API_URL}/${endpoint}`;
   const headers = new Headers();
   headers.set(
@@ -17,4 +35,4 @@ const req = async (endpoint, method, body = {}) => {
   return response.json();
 };
 
-module.exports = { req };
+module.exports = { getReq, postReq };
--- listeners/commands/profile.js
+++ listeners/commands/profile.js
@@ -1,19 +1,20 @@
-const { req } = require("../../helpers/request");
+const { getReq } = require("../../helpers/request");
 
 const profileCommandCallback = async ({ ack, respond, client }) => {
   try {
     await ack();
+    await respond("testing1");
     // get student email from slack
-    const email = client.users.profile.get("email");
+    const email = await client.users.profile.get("email");
     console.log("email", email);
     const endpoint = `studentsData?filter={"student_email":"${email}"}}`;
-    const response = await req(endpoint, "GET");
+    const response = await getReq(endpoint);
     const student = response.data[0];
     console.log("student", student);
     // get student from classe365
     // if found, save it on db
     // if not, ask student to update email at classe365, same as in slack
-    await respond("testing");
+    await respond("testing2");
     return;
   } catch (error) {
     console.error(error);
