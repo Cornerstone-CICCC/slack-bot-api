1aa72f5ff82cbcfe604919e9cbbc647c721a76c7
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-12-13 20:39:26 -0800

    test get student classe365

--- listeners/commands/profile.js
+++ listeners/commands/profile.js
@@ -4,20 +4,16 @@ const profileCommandCallback = async ({ ack, respond, client, payload }) => {
   try {
     await ack();
     await respond("testing1");
-    // get student email from slack
-    console.log("payload", payload);
-    console.log("user_id", payload.user_id);
     const info = await client.users.info({
       user: payload.user_id,
     });
-    console.log("info", info);
-    // const endpoint = `studentsData?filter={"student_email":"${email}"}}`;
-    // const response = await getReq(endpoint);
-    // const student = response.data[0];
-    // console.log("student", student);
-    // get student from classe365
-    // if found, save it on db
-    // if not, ask student to update email at classe365, same as in slack
+    // const email = info.user.profile.email;
+    const email = "head.tech@ciccc.ca";
+    const endpoint = `studentsData?filter={"student_email":"${email}"}}`;
+    const response = await getReq(endpoint);
+    const student = response.data[0];
+    console.log("student", student);
+
     await respond("testing2");
     return;
   } catch (error) {
