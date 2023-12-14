72cc73a7174e1199f6903c739125279255fb9c80
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-12-13 18:24:29 -0800

    remove button action

--- listeners/actions/index.js
+++ listeners/actions/index.js
@@ -1,7 +1,5 @@
 const { sampleActionCallback } = require("./sample-action");
-const { buttonAbcCallback } = require("./button-abc");
 
 module.exports.register = (app) => {
   app.action("sample_action_id", sampleActionCallback);
-  app.action("button_abc", buttonAbcCallback);
 };
