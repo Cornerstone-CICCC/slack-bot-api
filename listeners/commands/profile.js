c45485ce3f065533d07438405e4804cde857acc4
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2023-12-21 12:11:51 -0800

    added dynamic info student profile

--- listeners/commands/profile.js
+++ listeners/commands/profile.js
@@ -1,29 +1,41 @@
 const { getReq } = require("../../helpers/request");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const profileCommandCallback = async ({ ack, respond, client, payload }) => {
   try {
     await ack();
-    await respond("testing1");
-    const info = await client.users.info({
-      user: payload.user_id,
-    });
+    // const info = await client.users.info({
+    //   user: payload.user_id,
+    // });
     // const email = info.user.profile.email;
     const email = "head.tech@ciccc.ca";
     const endpoint = `studentsData?filter={"student_email":"${email}"}`;
     const response = await getReq(endpoint);
     if (response.data.length === 0) {
       await respond(
-        "I don't think I remember you. Please make sure your email on Slack is the same as the one on classe365."
+        "I don't think I remember you. Please make sure your email on Slack is the same as the one on Classe365."
       );
       return;
     }
     const student = response.data[0];
-    await respond(`Hello ${student.student_name}!
-        Your student ID is ${student.student_id}.`);
-    // const student = response.data[0];
-    // console.log("student", student);
+    console.log("admission number", student.admission_number);
+    console.log("text_105", student.text_105);
+    const {
+      student_name,
+      admission_number,
+      select_117: program,
+      select_129: start_date,
+      select_128: completion_date,
+      textarea_132: advisor_name,
+    } = student;
+    await respond(`Hello, ${student_name}!
+        Your student ID is ${admission_number}.
+        You are enrolled in ${program}.
+        The start date is ${start_date}.
+        The completion date is ${completion_date}.
+        Your student advisor is ${advisor_name}.`);
 
-    await respond("testing2");
     return;
   } catch (error) {
     console.error(error);
