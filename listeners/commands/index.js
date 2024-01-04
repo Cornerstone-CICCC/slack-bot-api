c2cac963094a3af4d09bd53bf3d3d84fff196904
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2024-01-04 13:22:21 -0800

    added commands

--- listeners/commands/index.js
+++ listeners/commands/index.js
@@ -1,12 +1,12 @@
-const { sampleCommandCallback } = require("./sample-command");
 const { profileCommandCallback } = require("./profile");
 const { calendarCallback } = require("./calendar");
 const { studentIdCallback } = require("./student-id");
 const { immigrationFaqCallback } = require("./immigration-faq");
 const { virtualAssistantCallback } = require("./virtual-assistant");
+const { refundCallback } = require("./refund");
+const { coopDocsCallback } = require("./coop-docs");
 
 module.exports.register = (app) => {
-  app.command("/sample-command", sampleCommandCallback);
   app.command("/calendar", calendarCallback);
   app.command("/profile", profileCommandCallback);
   app.command("/student-id", studentIdCallback);
@@ -16,4 +16,6 @@ module.exports.register = (app) => {
   app.command("/transcript", virtualAssistantCallback);
   app.command("/loa", virtualAssistantCallback);
   app.command("/loe", virtualAssistantCallback);
+  app.command("/refund", refundCallback);
+  app.command("/coop-docs", coopDocsCallback);
 };
