66c3ced2472be8a2c2217082fe477d7a3d92cf96
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2024-01-06 17:05:19 -0800

    fix profile

--- helpers/student/getStudentFromAPI.js
+++ helpers/student/getStudentFromAPI.js
@@ -3,6 +3,9 @@ const { getReq } = require("../utils/request");
 const getStudentFromAPI = async (studentEmail) => {
   const endpoint = `studentsData?filter={"student_email":"${studentEmail}"}`;
   const response = await getReq(endpoint);
+  if (!response.data.length) {
+    return null;
+  }
   return response.data[0];
 };
 
