c29adc71cf089fdb4ec911c6915b0cc5826c75e1
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2024-01-08 12:53:20 -0800

    fix issue response

--- helpers/grades/getGradesFromAPI.js
+++ helpers/grades/getGradesFromAPI.js
@@ -3,7 +3,7 @@ const { getReq } = require("../utils/request");
 const getGradesFromAPI = async (studentClasseId) => {
   // const response = await getReq(`studentScore?acds_id=1&id=7786`);
   const response = await getReq(`studentScore?acds_id=1&id=${studentClasseId}`);
-  return [...response.data.data];
+  return [...response.data];
 };
 
 module.exports = { getGradesFromAPI };
